730aa9cb2000553c1c4a751d21c2247303770fcc
dimaniagara <ds.sugak@gmail.com>
2022-09-06 06:38:44 -0400

    Updates 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,3 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
 import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
